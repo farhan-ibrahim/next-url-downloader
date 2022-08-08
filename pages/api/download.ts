d42c1663ee26cdb40b38ae25bed356b1f3a2f880
farhan-ibrahim <farhan@invokeisdata.com>
2022-08-08 15:36:57 +0800

    🎨  fix : Handle redundant file

--- pages/api/download.ts
+++ pages/api/download.ts
@@ -4,11 +4,12 @@ import * as ftp from "basic-ftp"
 import https from "https";
 import http from "http";
 import fs from "fs";
+import path from 'node:path';
 
 
 type Options = {
-    retries?: number
-    location?: string
+    retries?: number;
+    location?: string;
 }
 
 type Params = {
@@ -17,7 +18,7 @@ type Params = {
 }
 
 type Data = {
-  uri?: string
+  data?: any
   message?: string
   error?: string
 }
@@ -26,8 +27,8 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-    const { urls , options} = req.body as Params;
-    const location = options.location || "./Downloads/";
+    const { urls , options  } = req.body as Params;
+    const location = options.location || "Downloads";
 
     if(urls.length <= 0){
         res.status(400).json({
@@ -36,10 +37,12 @@ export default function handler(
         return;
     }
 
-    const downloadFTP = async (url:URL, writeStream:string , fileName:string ) => {
+    const downloadFTP = async (url:URL, folder:string , fileName:string ) => {
         return new Promise(async (resolve, reject) => {
             const client = new ftp.Client()
-            client.ftp.verbose = true
+            client.ftp.verbose = true;
+            const destination = path.resolve(folder, fileName);
+
     
             try {
                 await client.access({
@@ -47,7 +50,7 @@ export default function handler(
                     'user': 'dlpuser' ,// url.username,
                     'password': 'rNrKYTX9g7z3RgJRmxWuGHbeu' //url.password,
                 });
-                await client.downloadTo(writeStream, fileName);
+                await client.downloadTo(destination, fileName);
                 resolve({
                     uri: url.href,
                     message:"ftp download complete"
@@ -62,22 +65,56 @@ export default function handler(
         })
     }
 
-    const downloadHTTPS = async (url:URL, writeStream:string , fileName:string ) => {
+    const downloadFile = (res:http.IncomingMessage , folder:string , fileName:string, callback:() => void) => {
+        const destination = path.resolve(folder, fileName);
+        const folderPath = path.resolve(folder);
+        const ext = path.extname(fileName);
+        const fileNameWithoutExt = path.basename(fileName, ext);
+
+        // Check if file already exists
+        if(fs.existsSync(destination) && fs.statSync(destination).isFile()) {
+            // Download file with different name
+            const tempName = `${fileNameWithoutExt}-${Date.now()}.${ext}`;
+            const newDestination = path.resolve(folder, tempName);
+            const writeStream = fs.createWriteStream(newDestination);
+            res.pipe(writeStream);
+
+            // Check if it's the same file
+            writeStream.on("finish", () => {
+                const newBuf = fs.readFileSync(newDestination);
+                const oldBuf = fs.readFileSync(destination);
+
+                if(newBuf.equals(oldBuf)) {
+                    // Remove file if it already exists
+                    fs.unlinkSync(destination);
+
+                    // Rename temporary file to destination
+                    fs.renameSync(newDestination, destination);
+                }
+            })
+            
+        // Check if folder exists. If not, create it.
+        }else if (!fs.existsSync(folderPath)) {
+            fs.mkdirSync(folderPath);
+        }
+        
+        const writeStream = fs.createWriteStream(destination);
+        res.pipe(writeStream);
+
+        writeStream.on("finish", () => {
+            writeStream.close(callback);
+           
+        });
+    }
+
+    const downloadHTTPS = async (url:URL, folder:string , fileName:string ) => {
         return new Promise((resolve, reject) => {
             try{
-                https.get(url, (res) => {
-                    const path = fileName;
-                    const writeStream = fs.createWriteStream(path);
-                    res.pipe(writeStream);
-    
-                    writeStream.on("finish", () => {
-                        writeStream.close();
-                    });
-                    resolve({
-                        uri: url.href,
-                        message:"https download complete"
-                    })
-                })
+                https.get(url, (res) => downloadFile(res, folder, fileName, () =>resolve({
+                    uri: url.href,
+                    message:"http download complete"
+                })))
+                  
             }catch(err) {
                 console.log(err)
                 reject()
@@ -85,21 +122,14 @@ export default function handler(
         })
     }
 
-    const downloadHTTP = async (url:URL, writeStream:string , fileName:string ) => {
+    const downloadHTTP = async (url:URL, folder:string , fileName:string ) => {
         return new Promise((resolve, reject) => {
             try{
                 http.get(url, (res) => {
-                    const path = fileName;
-                    const writeStream = fs.createWriteStream(path);
-                    res.pipe(writeStream);
-    
-                    writeStream.on("finish", () => {
-                        writeStream.close();
-                    });
-                    resolve({
+                    downloadFile(res, folder, fileName, () =>resolve({
                         uri: url.href,
                         message:"http download complete"
-                    })
+                    }))
                 })
             }catch(err) {
                 console.log(err)
@@ -108,11 +138,11 @@ export default function handler(
         })
     }
         
-
-
-    const downloadFile = async (url:string) => {
+ 
+    const actions = urls.map(async (url:string) => {
         let u = new URL(url);
-        const fileName = u.pathname.split('/').pop() || "index.html";
+        const fileName = path.basename(url);
+
         switch(u.protocol){
             case "ftp:": return await downloadFTP(u , location , fileName) ;
             case "https:": return await downloadHTTPS(u , location , fileName) ; 
@@ -122,20 +152,25 @@ export default function handler(
                 error: 'protocol is not supported'
             }))
         }
-    }
- 
-    const actions = urls.map(downloadFile)
 
-    Promise.all(actions).then(results => {
-        console.log(results)
+       
+    })
+
+    let tries = options.retries || 0;
+    Promise.all(actions).then(data => {
         res.status(200).json({ 
-            uri: 'https://test.com' ,
-            message: 'download complete'
+            data,
+            message: 'download completed'
         })
     }).catch(err => {
-        console.log(err)
-        res.status(500).json({
-            error: 'download failed'
-        })
-    })
+        if(tries > 0){
+            tries--;
+            return handler(req, res);
+        }else{
+            res.status(500).json({
+                error: 'download failed' + err
+            })
+        }
+        
+    })  
 }
