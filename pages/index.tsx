9a80337b7c5c0153ce9096b407c56f3db408f80d
farhan-ibrahim <farhan@invokeisdata.com>
2022-08-09 13:54:57 +0800

    💄 fix : index UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,26 +6,14 @@ import styles from '../styles/Home.module.css'
 import Item from './components/Item'
 
 const Home: NextPage = () => {
-  // Some requirements.
-  // 1. The program can accept a single URI or a list of URIs to download [/]
-  // 2. It should be possible to configure download location as well as number of retries.
-  // 2. It should support HTTP/HTTPS, FTP and SFTP [/]
-  // 3. It should be extensible. Please pay attention to how new protocols can be added
-  // 4. It should handle retries and partial downloads. If a file fails to fully download then the partial files must be deleted
-  // 5. It should support parallel downloads [/]
-  // 6. It should handle name clashes. If two different resources have the same name, both should download correctly. If the same resource is downloaded twice, it should overwrite the previous one
-  // 7. Program architecture is important.
-  // 8. Don't forget about tests.
-
   const httpURL = 'http://techslides.com/demos/sample-videos/small.mp4';
   const httpsURL = 'https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg';
   const ftpURL = 'ftp://ftp.dlptest.com/';
   const sftpURL = 'sftp://itcsubmit.wustl.edu';
 
-
   const [ urls, setUrls ] = useState<string[]>(["",]);
   const [ retries , setRetries ] = useState<number>(0);
-  const [ location , setLocation ] = useState<string>('');
+  const [ location , setLocation ] = useState<string>('downloads');
   const [ progress , setProgress] = useState<number>(0);
 
   const onAddNewUrl = () => {
@@ -36,37 +24,26 @@ const Home: NextPage = () => {
     setUrls(urls.slice(0, urls.length - 1));
   }
 
-  const onClickDownload = async () => {
-    // axios({
-    //     url: 'http://api.dev/file-download', //your url
-    //     method: 'GET',
-    //     responseType: 'blob', // important
-    // }).then((response) => {
-    //     const url = window.URL.createObjectURL(new Blob([response.data]));
-    //     const link = document.createElement('a');
-    //     link.href = url;
-    //     link.setAttribute('download', 'file.pdf'); //or any other extension
-    //     document.body.appendChild(link);
-    //     link.click();
-    // });
-
-    fetch('/api/download',{
-      method: 'POST',
-      headers:{
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        urls: urls,
-        options:{
-          retries,
-          location,
-        }
-      }),
-    }).then((res) => 
-      res.json()
-    ).then((data) => {
-      console.log("result" , data);
-    })
+  const onClickDownload = async () => { 
+    try{
+      const res = await fetch('/api/download',{
+        method: 'POST',
+        headers:{
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          urls,
+          options:{
+            retries,
+            location,
+          }
+        }),
+      })
+  
+      const data = await res.json();
+    }catch(err){
+      console.error(err)
+    }
   }
 
   return (
@@ -102,18 +79,22 @@ const Home: NextPage = () => {
             />
           )
         })}
-        <label >Folder Name</label>
-        <input 
-          className={styles.input}
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-        <label >No of retries :</label>
-        <input 
-          className={styles.input}
-          value={retries.toString()}
-          onChange={e => setRetries(Number(e.target.value))}
-        />
+        <div className={styles.item}>
+          <label >Folder Name</label>
+          <input 
+            className={styles.input}
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label >No of retries :</label>
+          <input 
+            className={styles.input}
+            value={retries.toString()}
+            onChange={e => setRetries(Number(e.target.value))}
+          />
+        </div>
         <button className={styles.button} onClick={onClickDownload} >Download</button>
         <button className={styles.button} >Pause</button>
         <button className={styles.button} >Cancel</button>
@@ -139,3 +120,4 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
